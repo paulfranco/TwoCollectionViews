db0f587a65845b211fb5fdfed127f1f7d31d3657
Paul Franco <mail@paulfran.co>
2020-08-27 00:56:35 -0700

    completed demo project

---
+++ TwoCollectionViews/Cell.swift
@@ -0,0 +1,16 @@
+//
+//  CellCollectionViewCell.swift
+//  TwoCollectionViews
+//
+//  Created by Paul Franco on 8/27/20.
+//
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageA: UIImageView!
+    @IBOutlet weak var labelA: UILabel!
+    @IBOutlet weak var imageB: UIImageView!
+    @IBOutlet weak var labelB: UILabel!
+}
--- TwoCollectionViews/ViewController.swift
+++ TwoCollectionViews/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
 
+    var imageArrayA = [UIImage(named: "jersey1"), UIImage(named: "jersey2"), UIImage(named: "jersey3"), UIImage(named: "jersey4"),UIImage(named: "jersey5"), UIImage(named: "jersey6")]
+    
+    var imageArrayB = [UIImage(named: "shoe1"), UIImage(named: "shoe2"), UIImage(named: "shoe3"), UIImage(named: "shoe4"),UIImage(named: "shoe5"), UIImage(named: "shoe6")]
+    
+    var labelA = ["Jersey 1", "jersey 2", "Jersey 3", "Jersey 4", "Jersey 5", "Jersey 6"]
+    
+    var labelB = ["Shoe 1", "Shoe 2", "Shoe 3", "Shoe 4", "Shoe 5", "Shoe 6"]
+    
+    @IBOutlet weak var collectionViewA: UICollectionView!
+    @IBOutlet weak var collectionViewB: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionViewA.dataSource = self
+        collectionViewA.delegate = self
+        collectionViewB.dataSource = self
+        collectionViewB.delegate = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == self.collectionViewA {
+            return imageArrayA.count
+        }
+        return imageArrayB.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == self.collectionViewA {
+            let cellA = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
+            cellA.imageA.image = imageArrayA[indexPath.row]
+            cellA.labelA.text = labelA[indexPath.row]
+            return cellA
+        } else {
+            let cellB = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
+            cellB.imageB.image = imageArrayB[indexPath.row]
+            cellB.labelB.text = labelB[indexPath.row]
+            return cellB
+        }
     }
 
-
+    
 }
 
